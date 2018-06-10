44dc879233f012bf5563e3f96f5cc547b9bd9032
chalupaul <me@chalupaul.com>
2018-06-10 18:39:21 -0500

    extending vips

--- content/frontend/web/_list.php
+++ content/frontend/web/_list.php
@@ -2,6 +2,7 @@
 
 function list_names($app_hostname) {
     # list all names
+    $names_url = "http://" + $names[mt_rand(0, count($names) - 1)] . "/api/names";
     $names_url = "http://" . $app_hostname . "/api/names";
     $raw_names = file_get_contents($names_url);
     $raw_names = str_replace('[', '', $raw_names);
---
+++ content/frontend/web/test.php
@@ -0,0 +1,7 @@
+<?php
+
+$a = ['one', 'two', 'three'];
+
+echo $a[mt_rand(0, count($a) - 1)];
+
+?>
