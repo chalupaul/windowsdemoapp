93c3e1d065c07ca00a9ebab5c74e0529e2294fef
chalupaul <me@chalupaul.com>
2018-06-10 23:31:45 -0500

    added retry loop

--- content/frontend/web/_list.php
+++ content/frontend/web/_list.php
@@ -5,10 +5,14 @@ require_once('./_curl.php');
 function list_names() {
     $raw_names = get_curl();
     # list all names
-    $raw_names = str_replace('[', '', $raw_names);
-    $raw_names = str_replace(']', '', $raw_names);
-    $raw_names = str_replace('"', '', $raw_names);
-    $names = explode(',', $raw_names);
-    return $names;
+    if ($raw_names == '') {
+        return($list_names());
+    } else {
+        $raw_names = str_replace('[', '', $raw_names);
+        $raw_names = str_replace(']', '', $raw_names);
+        $raw_names = str_replace('"', '', $raw_names);
+        $names = explode(',', $raw_names);
+        return $names;
+    }
 }
 ?>
