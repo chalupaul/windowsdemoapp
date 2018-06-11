4763b6fcbcbc3ef70e2454fafbd35bd271a4f9ad
chalupaul <me@chalupaul.com>
2018-06-10 22:10:08 -0500

    moving to distributed backend apps

--- content/frontend/web/_list.php
+++ content/frontend/web/_list.php
@@ -1,10 +1,26 @@
 <?php
 require_once('./_config.php');
 
-function list_names($app_hostname) {
-    # list all names
+function list_names($app_ip) {
+    global $app_hostname;
+    $options = array(
+        CURLOPT_RETURNTRANSFER => true,   // return web page
+        CURLOPT_HEADER         => false,  // don't return headers
+        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+        CURLOPT_ENCODING       => "",     // handle compressed
+        CURLOPT_USERAGENT      => "frontend", // name of client
+        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+        CURLOPT_CONNECTTIMEOUT => 1,    // time-out on connect
+        CURLOPT_TIMEOUT        => 120,    // time-out on response
+        CURLOPT_RESOLVE        => array($app_hostname:80:$app_ip);
+    ); 
     $names_url = "http://" . $app_hostname . "/api/names";
-    $raw_names = file_get_contents($names_url);
+    $ch = curl_init($names_url);
+    curl_setopt_array($ch, $options);
+    $raw_names = curl_exec($ch);
+    curl_close($ch);
+    # list all names
     $raw_names = str_replace('[', '', $raw_names);
     $raw_names = str_replace(']', '', $raw_names);
     $raw_names = str_replace('"', '', $raw_names);
